9382c9e9c47009b660fca25a140107ea945678b5
Priyam Gupta <priyamgupta2508@gmail.com>
2024-12-06 14:49:04 +0530

    Update Reverse the array.cpp

--- Arrays/Reverse the array.cpp
+++ Arrays/Reverse the array.cpp
@@ -1,1 +1,12 @@
+void reverseArray(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        int left = 0;        // Start from the first index
+        int right = n - 1;   // Start from the last index
 
+        while (left < right) {
+            swap(arr[left], arr[right]);  // Swap the elements at left and right
+            left++;   // Move left pointer to the right
+            right--;  // Move right pointer to the left
+        }
+    }
